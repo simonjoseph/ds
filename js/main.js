17c28f5dd996259655370a8bb2b76944f3ad7890
simonJoseph <50570879+simonjoseph@users.noreply.github.com>
2022-06-19 15:25:59 +0100

    new

--- js/main.js
+++ js/main.js
@@ -147,7 +147,7 @@ $(function () {
         loop: true
         , margin: 30
         , mouseDrag: true
-        , autoplay: false
+        , autoplay: true
         , dots: false
         , nav: true
         , navText: ['<i class="fas fa-left" aria-hidden="true"></i>', '<i class="fas fa-right" aria-hidden="true"></i>']
@@ -170,20 +170,20 @@ $(function () {
         loop:true,
         margin: 30,
         mouseDrag:true,
-        autoplay:false,
+        autoplay:true,
         dots: true,
-        nav: false,
-        navText: ["<span class='lnr fas fa-angle-left'></span>","<span class='lnr fas fa-angle-right'></span>"],
+        // nav: false,
+        // navText: ["<span class='lnr fas fa-angle-left'></span>","<span class='lnr fas fa-angle-right'></span>"],
         responsiveClass:true,
         responsive:{
             0:{
-                items:1,
+                items:2,
             },
             600:{
-                items:1
+                items:3
             },
             1000:{
-                items:1
+                items:4
             }
         }
     });
@@ -212,26 +212,28 @@ $(function () {
     });
 
     // Team owlCarousel
+    $(document).ready(function(){
     $('.team .owl-carousel').owlCarousel({
-        loop: true
-        , margin: 30
-        , mouseDrag: true
-        , autoplay: false
-        , dots: false
-        , responsiveClass: true
-        , responsive: {
-            0: {
-                items: 1
-                , dots: true
-            , }
-            , 600: {
-                items: 2,
-                dots: true
-            }
-            , 1000: {
-                items: 3
+        loop:true,
+        margin: 30,
+        mouseDrag:true,
+        autoplay:true,
+        dots: true,
+        // nav: false,
+        // navText: ["<span class='lnr fas fa-angle-left'></span>","<span class='lnr fas fa-angle-right'></span>"],
+        responsiveClass:true,
+        responsive:{
+            0:{
+                items:2,
+            },
+            600:{
+                items:3
+            },
+            1000:{
+                items:5
             }
         }
+    });
     });
     
     // Blog owlCarousel
@@ -247,7 +249,7 @@ $(function () {
                 items: 1
             , }
             , 600: {
-                items: 2
+                items: 1
             }
             , 1000: {
                 items: 2
@@ -332,6 +334,63 @@ $(function () {
     
 });
 
+var TxtType = function(el, toRotate, period){
+    this.toRotate = toRotate;
+    this.el = el;
+    this.loopNum = 0;
+    this.period = parseInt(period, 10) || 2000;
+    this.txt = '';
+    this.tick();
+    this.isDeleting = false;
+};
+
+TxtType.prototype.tick = function(){
+    var i = this.loopNum % this.toRotate.length;
+    var fullTxt = this.toRotate[i];
+
+    if(this.isDeleting){
+        this.txt = fullTxt.substring(0, this.txt.length - 1);
+    } else{
+        this.txt = fullTxt.substring(0, this.txt.length + 1);
+    }
+
+    this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+
+    var that = this;
+
+    var delta = 200 - Math.random() * 100;
+
+    if(this.isDeleting){ delta /= 2; }
+
+    if(!this.isDeleting && this.txt === fullTxt ){
+        delta = this.period;
+        this.isDeleting = true;
+    } else if(this.isDeleting && this.txt === '' ){
+        this.isDeleting = false;
+        this.loopNum++;
+        delta = 500;
+    }
+
+    setTimeout(() => {
+        that.tick();
+    }, delta);
+}
+
+// window.onload = function(){
+//     var elements = document.getElementsByClassName('typewrite');
+//     for(var i=0; i<elements.length; i++ ){
+//         var toRotate = elements[i].getAttribute('data-type');
+//         var period = elements[i].getAttribute('data-period');
+//         if(toRotate){
+//             new TxtType(elements[i], JSON.parse(toRotate), period);
+//         }
+//     }
+//     var css = document.createElement("style");
+//     css.type = "text/css";
+//     css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
+//     document.body.appendChild(css);
+// }
+
 
 // Contact Form
     var form = $('.contact__form'),
@@ -365,4 +424,103 @@ $(function () {
         .done(done_func)
         .fail(fail_func);
     });
-    
+
+    // k
+let hideNavBarAllowed = 0; //only allowed when 1; Begins with 0 and will have +1 after 1 second.
+// Execute some code when the site is opened
+window.onload = function(){
+    var elements = document.getElementsByClassName('typewrite');
+    for(var i=0; i<elements.length; i++ ){
+        var toRotate = elements[i].getAttribute('data-type');
+        var period = elements[i].getAttribute('data-period');
+        if(toRotate){
+            new TxtType(elements[i], JSON.parse(toRotate), period);
+        }
+    }
+    var css = document.createElement("style");
+    css.type = "text/css";
+    css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
+    document.body.appendChild(css);
+    setTimeout(function () {
+        hideNavBarAllowed++; //true
+    },1000);
+
+    if (location.hash) {
+        //Ensure after 1000 ms
+        setTimeout(function () {
+            goToAndShowNavbar(location.hash);
+        },1100);
+    }
+
+    // Lazy load for some images
+
+    setTimeout(function () {
+        let lazyImages = document.getElementsByClassName("vini-lazy-img");
+        for(let i=0; i<lazyImages.length; i++){
+
+            if(lazyImages[i].hasAttribute("data-lazy-src")){
+                let realImgSrc = lazyImages[i].getAttribute("data-lazy-src");
+
+                let parentElement = lazyImages[i].parentElement;
+
+                if(parentElement && parentElement.tagName.toLowerCase() == "picture"){
+                    addSourceSetsToPicture(parentElement, realImgSrc);
+                }
+
+                lazyImages[i].setAttribute("src", realImgSrc);
+                lazyImages[i].removeAttribute("data-lazy-src");
+            }
+        }
+    },10);
+};
+
+function addSourceSetsToPicture(pictureElement, realImgSrc) {
+    console.log("addSourceSetsToPicture entrou = "+realImgSrc);
+
+    let indexSlash = realImgSrc.lastIndexOf("/");
+    let imagePath = realImgSrc.substring(0, indexSlash);
+    let imageName = realImgSrc.substring(indexSlash+1);
+
+    let indexDot = imageName.lastIndexOf(".");
+    let imageExt = imageName.substring(indexDot+1);
+    imageName = imageName.substring(0, indexDot);
+
+    let webpSourceElem = document.createElement('source');
+    webpSourceElem.srcset = imagePath+"/webp/"+imageName+".webp";
+    webpSourceElem.type = "image/webp";
+
+    let jpgOrPngSourceElem = document.createElement('source');
+    jpgOrPngSourceElem.srcset = realImgSrc;
+    jpgOrPngSourceElem.type = imageExt.toLowerCase()==="png" ? "image/png" : "image/jpeg";
+
+    let imgElement = pictureElement.firstChild;
+    pictureElement.insertBefore(webpSourceElem, imgElement);
+    pictureElement.insertBefore(jpgOrPngSourceElem, imgElement);
+}
+// Go to anchor (hash) and ensure that Navbar will be showing and not hide
+function goToAndShowNavbar(href) {
+    hideNavBarAllowed--; //false
+    location.href = href;
+    showNavbar();
+
+    setTimeout(function () {
+        hideNavBarAllowed++; //true
+    }, 1000);
+
+    return false; //so the original anchor href will have no effect
+}
+
+//Swiper Carousell
+const swiper = new Swiper('.swiper-container', {
+    // Optional parameters
+    loop: true,
+    autoplay: {
+        delay: 3000,
+    },
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+});
